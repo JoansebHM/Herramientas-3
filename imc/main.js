7348bba7f390c64ec5eb1638e95dd9c0208d06aa
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2023-09-12 13:41:47 -0500

    Add changes

--- imc/main.js
+++ imc/main.js
@@ -7,16 +7,28 @@ imc_button.addEventListener("click", function() {
 
 function calculate_imc() {
     const img_default = document.getElementById("div-img-default")
-    img_default.style.display = "None"
     const new_img = document.getElementById("div-img")
-    new_img.style.display = "block"
     const altura = parseFloat(document.getElementById("altura_input").value);
     const peso = parseFloat(document.getElementById("peso_input").value);
+    const show_img_imc = document.getElementById("img-section")
 
     if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
-        resultado.value = "Por favor, ingresa valores válidos para altura y peso.";
+        resultado.value = "Ingrese valores validos";
+        new_img.style.display = "none"
+        img_default.style.display = "block"
     } else {
+        img_default.style.display = "None"
+        new_img.style.display = "block"
         const imc = peso / (altura ** 2);
         resultado.value = imc.toFixed(2);
+        if (imc < 18.5){
+            show_img_imc.src = "./img/peso_insuficiente.png"
+        } else if ((imc > 18.5) && (imc <= 24.9)){
+            show_img_imc.src = "./img/peso_saludable.png"
+        } else if ((imc > 25.0) && (imc <= 29.9)){
+            show_img_imc.src = "./img/sobrepeso.png"
+        } else {
+            show_img_imc.src = "./img/obesidad.png"
+        }
     }
 }
