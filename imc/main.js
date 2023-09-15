501dcd2be7cec912121ed13c14537db58a50665b
cepillin123 <joan.herrera481@pascualbravo.edu.co>
2023-09-15 14:18:28 -0500

    Add Changes

--- imc/main.js
+++ imc/main.js
@@ -1,38 +1,48 @@
-const resultado = document.getElementById("imc_input");
-const imc_button = document.getElementById("imc_button");
+document.addEventListener("DOMContentLoaded", function() {
+    // Tu código JavaScript aquí
+    const resultado = document.getElementById("imc_input");
+    const imc_button = document.getElementById("imc_button");
+    const bajo = document.querySelector(".row-img1");
+    const normal = document.querySelector(".row-img2");
+    const sobrepeso = document.querySelector(".row-img3");
+    const obesidad = document.querySelector(".row-img4");
+    const rowimg1 = bajo.classList.value;
+    const rowimg2 = normal.classList.value;
+    const rowimg3 = sobrepeso.classList.value;
+    const rowimg4 = obesidad.classList.value;
 
-imc_button.addEventListener("click", function() {
-    calculate_imc();
-});
+    imc_button.addEventListener("click", function() {
+        bajo.className = rowimg1;
+        normal.className = rowimg2;
+        sobrepeso.className = rowimg3;
+        obesidad.className = rowimg4;
+        calculate_imc();
+    });
 
-function calculate_imc() {
-    const img_default = document.getElementById("div-img-default")
-    const new_img = document.getElementById("div-img")
-    const altura = parseFloat(document.getElementById("altura_input").value);
-    const peso = parseFloat(document.getElementById("peso_input").value);
-    const show_img_imc = document.getElementById("img-section")
-
-    if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
-        resultado.value = "Ingrese valores validos";
-        new_img.style.display = "none"
-        img_default.style.display = "block"
-    } else {
-        img_default.style.display = "None"
-        new_img.style.display = "block"
-        const imc = peso / (altura ** 2);
-        resultado.value = imc.toFixed(2);
-        if (imc < 18.5){
-            show_img_imc.classList.remove("img-sobrepeso")
-            show_img_imc.src = "./img/peso_insuficiente.png"
-        } else if ((imc > 18.5) && (imc < 25)){
-            show_img_imc.classList.remove("img-sobrepeso")
-            show_img_imc.src = "./img/peso_saludable.png"
-        } else if ((imc > 25.0) && (imc < 29.9)){
-            show_img_imc.classList.add("img-sobrepeso")
-            show_img_imc.src = "./img/sobrepeso.png"
+    function calculate_imc() {
+        const altura = parseFloat(document.getElementById("altura_input").value);
+        const peso = parseFloat(document.getElementById("peso_input").value);
+    
+        if (isNaN(altura) || isNaN(peso) || altura <= 0 || peso <= 0) {
+            resultado.value = "Ingrese valores validos";
         } else {
-            show_img_imc.classList.remove("img-sobrepeso")
-            show_img_imc.src = "./img/obesidad.png"
+            const imc = peso / (altura ** 2);
+            resultado.value = imc.toFixed(2);
+            if (imc < 18.5){
+                bajo.classList.remove("row-img1") 
+                bajo.classList.add("blur-off")
+            } else if ((imc > 18.5) && (imc < 25)){
+                normal.classList.remove("row-img2") 
+                normal.classList.add("blur-off")
+            } else if ((imc > 25.0) && (imc < 29.9)){
+                sobrepeso.classList.remove("row-img3") 
+                sobrepeso.classList.add("blur-off")
+            } else {
+                obesidad.classList.remove("row-img4") 
+                obesidad.classList.add("blur-off")
+            }
         }
     }
-}
+    
+});
+
